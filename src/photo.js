50e5aac0dcb806908e5136a8a707cb2abd698303
yth98 <b06901031@ntu.edu.tw>
2019-05-03 21:05:45 +0800

    try on react-photo-gallery

--- src/photo.js
+++ src/photo.js
@@ -1,24 +1,86 @@
 import React from "react";
 import { render } from "react-dom";
 import Gallery from "react-photo-gallery";
+//import Test1 from './images/1-by-1.png';
+
+// https://stackoverflow.com/questions/44607396/importing-multiple-files-in-react
+/*
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+
+const images = importAll(require.context('./images', false, '/\.(png|jpe?g|svg)$/'));
+*/
 
 const photos = [
     {
-        src:"https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-9/59244100_1210569819111661_7784539391191416832_o.jpg?_nc_cat=102&_nc_ht=scontent-hkg3-1.xx&oh=5279a951f7de1ddadf894e6acd795448&oe=5D372605",
-        width:1,
-        height:1
+        src:"https://i.imgur.com/ZcOqzJs.png",
+        width:589,
+        height:1064
     },
     {
-        src:"https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-9/58622692_1210569402445036_285859343750922240_o.jpg?_nc_cat=104&_nc_ht=scontent-hkg3-1.xx&oh=49073590d25075c0fa2a265ad9ffe7d1&oe=5D723CF6",
-        width:3,
-        height:4
+        src:"https://i.imgur.com/tzgtYRD.png",
+        width:1358,
+        height:885
     },
     {
-        src:"https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-0/q90/c50.0.200.200a/p200x200/59380274_1210564609112182_4427657342787518464_n.jpg?_nc_cat=100&_nc_ht=scontent-hkg3-1.xx&oh=d69e58b39fb6075a79c24579828ec2da&oe=5D6B34E8",
-        width:3,
-        height:4
+        src:"https://i.imgur.com/GJI0Ob1.png",
+        width:1300,
+        height:885
+    },
+    {
+        src:"https://i.imgur.com/yDS61Nq.png",
+        width:766,
+        height:708
+    },
+    {
+        src:"https://i.imgur.com/gXcmfGn.png",
+        width:1533,
+        height:885
+    },
+    {
+        src:"https://i.imgur.com/m97LE24.png",
+        width:710,
+        height:708
+    },
+    {
+        src:"https://i.imgur.com/m1ISSUu.png",
+        width:885,
+        height:885
+    },
+    {
+        src:"https://i.imgur.com/2sjKb0D.png",
+        width:1241,
+        height:708
+    },
+    {
+        src:"https://i.imgur.com/tzHndL7.png",
+        width:1181,
+        height:1179
+    },
+    {
+        src:"https://i.imgur.com/jrpZlLQ.png",
+        width:1062,
+        height:1062
+    },
+    {
+        src:"https://i.imgur.com/G9KQMjK.png",
+        width:1241,
+        height:708
+    },
+    {
+        src:"https://i.imgur.com/t00gBh8.png",
+        width:1239,
+        height:766
+    },
+    {
+        src:"https://i.imgur.com/675zS27.png",
+        width:1479,
+        height:885
     }
 ];
 
 const BasicRows = () => <Gallery photos={photos} />;
-render(<BasicRows />, document.getElementById("app"));
+render(<BasicRows />, document.getElementById("root"));
