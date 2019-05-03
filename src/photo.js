746973a3cf11702efdc92dbf3f9650833c6fecce
yth98 <b06901031@ntu.edu.tw>
2019-05-03 21:30:00 +0800

    more photos

--- src/photo.js
+++ src/photo.js
@@ -79,6 +79,56 @@ const photos = [
         src:"https://i.imgur.com/675zS27.png",
         width:1479,
         height:885
+    },
+    {
+        src:"https://i.imgur.com/RBtiSCN.png",
+        width:589,
+        height:887
+    },
+    {
+        src:"https://i.imgur.com/CMixdiK.png",
+        width:1418,
+        height:1064
+    },
+    {
+        src:"https://i.imgur.com/zZ7lg9H.png",
+        width:1062,
+        height:1062
+    },
+    {
+        src:"https://i.imgur.com/fnCNNVQ.png",
+        width:1652,
+        height:885
+    },
+    {
+        src:"https://i.imgur.com/AAyT4fF.png",
+        width:1772,
+        height:1179
+    },
+    {
+        src:"https://i.imgur.com/GV6SDUw.png",
+        width:1183,
+        height:1066
+    },
+    {
+        src:"https://i.imgur.com/pCCRx9w.png",
+        width:1210,
+        height:1062
+    },
+    {
+        src:"https://i.imgur.com/jvYZv3L.png",
+        width:1239,
+        height:1179
+    },
+    {
+        src:"https://i.imgur.com/edwnV7Q.png",
+        width:708,
+        height:1062
+    },
+    {
+        src:"https://i.imgur.com/rZ9mVeC.png",
+        width:887,
+        height:591
     }
 ];
 
